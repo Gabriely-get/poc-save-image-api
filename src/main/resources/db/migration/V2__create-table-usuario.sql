5be42697a8c6c3f4c86f2d9ccc4c1d8795522d67
Gabriely-get <gabrielysantos.informatica@gmail.com>
2023-11-16 00:30:18 -0300

    feat: create table usuario

---
+++ src/main/resources/db/migration/V2__create-table-usuario.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Usuario (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    imageid BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (imageid) REFERENCES Image(id)
+);
